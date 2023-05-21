3ab9e84eba2aa3fef9eca2302d4682d32a23d09b
Juliano <julianosoder1989@gmail.com>
2023-05-21 00:10:48 -0300

    configs originais

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Notificationchema } from './notification/schema/notification.schema';
 import { AdminMiddleware } from './utils/adminMidleware';
 
-//mongodb://soder:soder1989@172.18.0.3:27017
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest'),
-    MongooseModule.forFeature([
-      { name: 'notification', schema: Notificationchema },
-    ]),
+    MongooseModule.forRoot('mongodb://soder:soder1989@172.18.0.3:27017', {
+      dbName: 'notificacao',
+    }),
+    MongooseModule.forFeature([{ name: 'notification', schema: Notificationchema }]),
     NotificationModule,
   ],
   controllers: [AppController],
@@ -20,6 +19,8 @@ import { AdminMiddleware } from './utils/adminMidleware';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(AdminMiddleware).forRoutes('admin');
+    consumer
+      .apply(AdminMiddleware)
+      .forRoutes('admin');
   }
 }
